5430e235695869e86806384be4e7ac1a7673fb2e
x23309709@student.ncirl.ie <x23309709@student.ncirl.ie>
2024-12-05 17:11:09 +0000

    Fixed Sone Minor Errors

--- Scripts/sidepanel.js
+++ Scripts/sidepanel.js
@@ -1,23 +1,19 @@
 function toggleMenu() {
-  console.log("Got inside the function")
 
   // Toggle the visibility of the side panel
   if (window.getComputedStyle(sidebar,null).getPropertyValue("display") =="flex") {
     sidebar.style.display = "none";
     iconbar.style.display = "flex";
-    mainContent.classList.add("fade-out");
 
   } else {
     sidebar.style.display = "flex";
     iconbar.style.display = "none";
-    mainContent.classList.remove("fade-out");
 
   }
 }
 
 var navButton = document.getElementById("navbutton");
 var navButton2 = document.getElementById("navbutton2");
-var mainContent = document.getElementById("main-content");
 var sidebar = document.getElementById("sidebar");
 var iconbar = document.getElementById("iconbar");
 
